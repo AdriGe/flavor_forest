2f64fba3e074548d1729f00190fddf6141f5a93a
Adrien <johndoe@example.com>
2024-01-28 14:09:17 +0100

    Add exception to handle refresh token and 401 in axios interceptors

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
 
 const baseURL = `${import.meta.env.VITE_API_BASE_URL}:${import.meta.env.VITE_API_PORT}`;
@@ -6,32 +7,48 @@ const baseURL = `${import.meta.env.VITE_API_BASE_URL}:${import.meta.env.VITE_API
 const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
-    console.log('Intercepting request');
     const token = localStorage.getItem('access_token');
     if (token) {
-        console.log('Adding token to request');
         config.headers.Authorization = `Bearer ${token}`;
     }
-    console.log('Request intercepted');
     return config;
 }, (error) => {
-    console.log('Error intercepting request');
     return Promise.reject(error);
 });
+
 
 api.interceptors.response.use(
     (response) => response,
     async (error) => {
-        console.log('Intercepting response');
         const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
-            console.log('Intercepted 401 error');
-            originalRequest._retry = true;
-            const authStore = useAuthStore();
-            await authStore.refreshToken();
-            return api(originalRequest);
+        if (error.response.status === 401) {
+            // Vérifiez si l'erreur provient de l'endpoint de rafraîchissement du token
+            if (originalRequest.url === '/users/refresh-token') {
+                localStorage.clear();
+                router.push('/login');
+                return Promise.reject(error);
+            }
+
+            if (!originalRequest._retry) {
+                originalRequest._retry = true;
+                const refreshToken = localStorage.getItem('refresh_token');
+                if (refreshToken) {
+                    try {
+                        const response = await api.post('/users/refresh-token', { refresh_token: refreshToken });
+                        localStorage.setItem('access_token', response.data.access_token);
+                        return api(originalRequest);
+                    } catch (refreshError) {
+                        localStorage.clear();
+                        router.push('/login');
+                        return Promise.reject(refreshError);
+                    }
+                } else {
+                    localStorage.clear();
+                    router.push('/login');
+                    return Promise.reject(error);
+                }
+            }
         }
-        console.log('Response intercepted');
         return Promise.reject(error);
     }
 );
